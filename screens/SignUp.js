90ee9b5143c549df64a428d7f849abd3f0f2341b
Stoica Andrei <stoica.andrei32@gmail.com>
2020-10-02 14:21:38 +0300

    Implemented firestore

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,55 +1,64 @@
-import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button, Image, Alert } from 'react-native'
-// import firebase from '@react-native-firebase/app'
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  Alert,
+} from 'react-native';
 import auth, {firebase} from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import App from '../App';
 
-
+firebase.initializeApp(App);
 
 export default class SignUp extends React.Component {
-  state = { email: '', password: '', errorMessage: null }
+  state = {email: '', password: '', errorMessage: null};
 
-  handleSignUp = async (email , password) => {
+  handleSignUp = async (email, password) => {
     try {
-      let response = await firebase.auth().createUserWithEmailAndPassword( this.state.email, this.state.password)
+      let response = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password);
       if (response) {
-        this.handleAdd();
-        Alert.alert('Success', 'Registration successfully')
+        this.handleFirestore();
+        Alert.alert('Success', 'Registration successfully');
       }
     } catch (e) {
-      console.error(e.message)
+      console.error(e.message);
     }
-  console.log('handleSignUp')
-}
+    console.log('handleSignUp');
+  };
 
-handleAdd = () => {
-  console.log('dada')
-  firebase
-  .firestore
-  .collection('users')
-  .add({
-    email: this.state.email,
-    password: this.state.password
-  })
-  .then(() => {
-    console.log('User added!')
-  })
-}
+  handleFirestore = () => {
+    console.log('dada');
+    const userDocument = firestore()
+      .collection('users')
+      .add({
+        email: this.state.email,
+        password: this.state.password,
+      })
+      .then(() => {
+        console.log('User added!');
+      });
+  };
 
-render() {
+  render() {
     return (
       <View style={styles.container}>
-                <Image style={styles.img} source={require('./skoda.png')}/>
+        <Image style={styles.img} source={require('./skoda.png')} />
 
         <Text>Sign Up</Text>
-        {this.state.errorMessage &&
-          <Text style={{ color: 'red' }}>
-            {this.state.errorMessage}
-          </Text>}
+        {this.state.errorMessage && (
+          <Text style={{color: 'red'}}>{this.state.errorMessage}</Text>
+        )}
         <TextInput
           placeholder="Email"
           autoCapitalize="none"
           style={styles.textInput}
-          onChangeText={email => this.setState({ email })}
+          onChangeText={(email) => this.setState({email})}
           value={this.state.email}
         />
         <TextInput
@@ -57,7 +66,7 @@ render() {
           placeholder="Password"
           autoCapitalize="none"
           style={styles.textInput}
-          onChangeText={password => this.setState({ password })}
+          onChangeText={(password) => this.setState({password})}
           value={this.state.password}
         />
         <Button title="Sign Up" onPress={this.handleSignUp} />
@@ -66,24 +75,24 @@ render() {
           onPress={() => this.props.navigation.navigate('Login')}
         />
       </View>
-    )
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   textInput: {
     height: 40,
     width: '90%',
     borderColor: 'gray',
     borderWidth: 1,
-    marginTop: 8
+    marginTop: 8,
   },
   img: {
     width: 150,
-    height: 150
-  }
-})
+    height: 150,
+  },
+});
